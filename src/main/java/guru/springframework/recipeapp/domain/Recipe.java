f879dcb8e6778f8dc16b60cc01c45f061e164c61
Orcun Sancar <eorcunsancar@gmail.com>
2021-01-05 23:06:05 +0300

    Ingredient Entity added and many to one relation added with Recipe

--- src/main/java/guru/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeapp/domain/Recipe.java
@@ -1,11 +1,14 @@
 package guru.springframework.recipeapp.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -24,6 +27,9 @@ public class Recipe {
 	private String directions;
 	//todo add
 	//private Difficulty difficulty;
+	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
 	
 	@Lob
 	private Byte[] image;
@@ -92,7 +98,12 @@ public class Recipe {
 	public void setNotes(Notes notes) {
 		this.notes = notes;
 	}
-	
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
 	
 	
 }
