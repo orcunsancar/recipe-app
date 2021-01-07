8827344b413521228fc524634f0ee5f9f5a20adf
Orcun Sancar <eorcunsancar@gmail.com>
2021-01-07 22:35:11 +0300

    Category and UOM repository findByDescription query added using Optional

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -1,13 +1,39 @@
 package guru.springframework.recipeapp.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.recipeapp.domain.Category;
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import guru.springframework.recipeapp.repositories.CategoryRepository;
+import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
+
 	@RequestMapping({"","/","/index"})
 	public String getIndexPage() {
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("Cat id is: " + categoryOptional.get().getId());
+		System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+		
 		return "index";
 	}
 }
--- src/main/java/guru/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package guru.springframework.recipeapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.recipeapp.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
 
+	Optional<UnitOfMeasure> findByDescription(String description);
+	
 }
