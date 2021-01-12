31c2699f44d1320072e2f47b2ac465304d4759f3
Orcun Sancar <eorcunsancar@gmail.com>
2021-01-12 22:23:06 +0300

    Lombok implementation added for Recipe and Category Entities

--- src/main/java/guru/springframework/recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/recipeapp/domain/Category.java
@@ -1,8 +1,14 @@
 package guru.springframework.recipeapp.domain;
 
 import javax.persistence.*;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.util.Set;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
 
@@ -13,28 +19,4 @@ public class Category {
 
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
