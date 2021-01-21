b5fbc58de7b3fb70a256fe90a0f81213d90464f1
Orcun Sancar <eorcunsancar@gmail.com>
2021-01-21 15:10:57 +0300

    Recipe findById added

--- src/main/java/guru/springframework/recipeapp/services/RecipeService.java
+++ src/main/java/guru/springframework/recipeapp/services/RecipeService.java
@@ -7,4 +7,6 @@ import guru.springframework.recipeapp.domain.Recipe;
 public interface RecipeService {
 
 	Set<Recipe> getRecipes();
+	
+	Recipe findById(Long l);
 }
--- src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipeapp.services;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
@@ -22,10 +23,21 @@ public class RecipeServiceImpl implements RecipeService {
 	@Override
 	public Set<Recipe> getRecipes() {
 		log.debug("I'm in the service");
-		
+
 		Set<Recipe> recipeSet = new HashSet<>();
 		recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 		return recipeSet;
+	}
+
+	@Override
+	public Recipe findById(Long l) {
+		Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+		if (!recipeOptional.isPresent()) {
+			throw new RuntimeException("Recipe Not Found!");
+		}
+
+		return recipeOptional.get();
 	}
 
 }
