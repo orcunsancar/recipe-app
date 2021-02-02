da5c39c0cde9f518278dd6968cdf4cb4c364f83f
orcunsancar <eorcunsancar@gmail.com>
2021-02-02 15:22:37 +0300

    Adding 404 error page

--- src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
@@ -52,14 +52,15 @@ public class RecipeControllerTest {
 				.andExpect(model().attributeExists("recipe"));
 	}
 
-	@Test
-	public void testGetRecipeNotFound() throws Exception {
+	 @Test
+	    public void testGetRecipeNotFound() throws Exception {
 
-		when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+	        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
-		mockMvc.perform(get("/recipe/1/show"))
-				.andExpect(status().isNotFound());
-	}
+	        mockMvc.perform(get("/recipe/1/show"))
+	                .andExpect(status().isNotFound())
+	                .andExpect(view().name("404error"));
+	    }
 
 	@Test
 	public void testGetNewRecipeForm() throws Exception {
