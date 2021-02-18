d33dd5c283879b07ac805cf2e11dc15b07f85d19
orcunsancar <eorcunsancar@gmail.com>
2021-02-18 16:50:53 +0300

    Displaying validation errors with messages

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package guru.springframework.recipeapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,7 +22,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class RecipeController {
-
+	
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService) {
@@ -43,14 +47,23 @@ public class RecipeController {
 	@GetMapping("recipe/{id}/update")
 	public String updateRecipe(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-		return "recipe/recipeform";
+		return RECIPE_RECIPEFORM_URL;
 	}
 
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+		}
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-		return "redirect:/recipe/" + savedCommand.getId() + "/show";
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
 	}
 
 	@GetMapping("recipe/{id}/delete")
--- src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
@@ -65,7 +65,7 @@ public class RecipeControllerTest {
     }
 	 
 	@Test
-    public void testGetRecipeNumberFormatFound() throws Exception {
+    public void testGetRecipeNumberFormatException() throws Exception {
 
         mockMvc.perform(get("/recipe/asdf/show"))
                 .andExpect(status().isBadRequest())
@@ -92,11 +92,29 @@ public class RecipeControllerTest {
 		mockMvc.perform(post("/recipe")
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.param("id", "")
-				.param("description", "some string"))
+				.param("description", "some string")
+				.param("directions", "some directions"))
 				.andExpect(status().is3xxRedirection())
 				.andExpect(view().name("redirect:/recipe/2/show"));
 	}
 
+	@Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
+    }
+	
 	@Test
 	public void testGetUpdateView() throws Exception {
 		RecipeCommand command = new RecipeCommand();
