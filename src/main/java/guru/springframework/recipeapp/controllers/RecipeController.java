59653e3afe28967374de02ad6852f3dee62f722d
orcunsancar <eorcunsancar@gmail.com>
2021-02-18 15:30:37 +0300

    Adding Number Format Exception

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -62,19 +62,35 @@ public class RecipeController {
 		return "redirect:/";
 	}
 	
-	 @ResponseStatus(HttpStatus.NOT_FOUND)
-	    @ExceptionHandler(NotFoundException.class)
-	    public ModelAndView handleNotFound(Exception exception){
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
 
-	        log.error("Handling not found exception");
-	        log.error(exception.getMessage());
-	       
-	        ModelAndView modelAndView = new ModelAndView();
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+       
+        ModelAndView modelAndView = new ModelAndView();
 
-	        modelAndView.setViewName("404error");
-	        modelAndView.addObject("exception",exception);
-	       
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception",exception);
+       
 
-	        return modelAndView;
-	    }
+        return modelAndView;
+    }
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+       
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception",exception);
+       
+
+        return modelAndView;
+    }
 }
--- src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
@@ -52,15 +52,23 @@ public class RecipeControllerTest {
 				.andExpect(model().attributeExists("recipe"));
 	}
 
-	 @Test
-	    public void testGetRecipeNotFound() throws Exception {
+	@Test
+    public void testGetRecipeNotFound() throws Exception {
 
-	        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
-	        mockMvc.perform(get("/recipe/1/show"))
-	                .andExpect(status().isNotFound())
-	                .andExpect(view().name("404error"));
-	    }
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
+    }
+	 
+	@Test
+    public void testGetRecipeNumberFormatFound() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 
 	@Test
 	public void testGetNewRecipeForm() throws Exception {
